b8d29793efdc51e2dc7cf04613af849b6650a7a3
Khizar Malik <k.malik@se17.qmul.ac.uk>
2019-02-17 21:37:57 +0000

    Background-added

--- react/src/Components/main.js
+++ react/src/Components/main.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "../Styles/nav.css";
+import "../Styles/main.css";
 
 class Main extends Component {
   state = {};
   render() {
     return (
-      <div>
+      <div class="full-width full-height background-image">
         <h1>main</h1>
       </div>
     );
