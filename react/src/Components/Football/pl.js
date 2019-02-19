f316eb6349b9994443272fd78876ee6c1e538809
Bilal Imran <bilalimran@bilals-mbp.wkstn.qmul.ac.uk>
2019-02-19 16:26:55 +0000

    my part

---
+++ react/src/Components/Football/pl.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class pl extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="title">
+        <h1>Premier League</h1>
+      </div>
+    );
+  }
+}
+
+export default pl;
---
+++ react/src/Components/MainFootball.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PL from "./Football/pl";
+import Result from "./Football/result";
+import Fixture from "./Football/fixture";
+import Table from "./Football/table";
+import Team from "./Football/team";
+import "../Styles/football.css";
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <PL />
+        <p> </p>
+        <Result />
+        <p> </p>
+        <Fixture />
+        <p> </p>
+        <Table />
+        <p> </p>
+        <Team />
+      </div>
+    );
+  }
+}
+
+export default Main;
