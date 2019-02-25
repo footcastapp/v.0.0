833f521079fff87da426bcf58210592ac613070a
Khizar Malik <k.malik@se17.qmul.ac.uk>
2019-02-25 23:50:08 +0000

    Current City added

--- react/src/Components/Main_screen_component/CurrentCity.js
+++ react/src/Components/Main_screen_component/CurrentCity.js
@@ -3,9 +3,64 @@ import InfoComponent from "./InfoComponent";
 import Daily from "./Daily";
 import HourlyTimeline from "./HourlyTimeline";
 import "../../Styles/CurrentCity.css";
+
 class CurrentCity extends Component {
-  state = { currentcity: "London", country: "United Kingdom" };
+  state = {
+    currentcity: "",
+    country: "",
+    apiKey: `ozf3wmtHsujhAkZPjibABY942wvPQCyh`,
+    cityKey: ""
+  };
+  constructor() {
+    super();
+    this.refreshCity = this.refreshCity.bind(this);
+    this.refreshTemperature = this.refreshTemperature.bind(this);
+    this.refreshCity();
+  }
+  refreshCity = () => {
+    const that = this;
+    $.ajax("http://ip-api.com/json").then(
+      function success(response) {
+        let { city, country, Key } = response;
+        that.setState({ currentcity: response.city });
+        that.setState({ country: response.country });
+      },
+
+      function fail(data, status) {
+        return null;
+      }
+    );
+  };
+  refreshTemperature = () => {
+    let that = this;
+    //console.log(state);
+
+    $.ajax(
+      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${
+        that.state.apiKey
+      }&q=${that.state.currentcity}`
+    ).then(
+      function success(response) {
+        let { Key } = response[0];
+        that.setState({ cityKey: Key });
+      },
+
+      function fail(data, status) {
+        return console.log("Number of access");
+      }
+    );
+  };
+
+  componentDidMount() {
+    console.log("Starting");
+    this.refreshTemperature();
+  }
+  //this.setState({ currentcity: location.city });
+  //this.setState({ country: location.country });
+  //}
+
   render() {
+    this.refreshTemperature();
     return (
       <div className="weather-container">
         <InfoComponent
