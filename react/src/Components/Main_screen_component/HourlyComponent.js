02fc9630cf385a67390d650feb776d6ca16189c3
Khizar Malik <k.malik@se17.qmul.ac.uk>
2019-02-26 18:33:39 +0000

    Hourly data added

--- react/src/Components/Main_screen_component/HourlyComponent.js
+++ react/src/Components/Main_screen_component/HourlyComponent.js
@@ -6,11 +6,26 @@ class HourlyComponent extends Component {
   render() {
     return (
       <div className="hourly-component">
-        <p>16:00</p>
-        <img src={sunny} alt="l" />
-        <p>11 ºC</p>
+        <p>{this.props.time}</p>
+        <img
+          src={this.decidePhoto(this.props.daylight, this.props.weather)}
+          alt="l"
+        />
+        <p>{this.props.temp} ºC</p>
       </div>
     );
+  }
+  decidePhoto(value, weather) {
+    let src = "../Images/";
+    if (value === true && weather == "Clear") {
+      src += "sunny.png";
+    } else if (value === false && weather == "Clear") {
+      src += "moon.png";
+    } else {
+      src += weather + ".png";
+    }
+    src = src.toString();
+    return src;
   }
 }
 
--- react/src/Components/Main_screen_component/HourlyTimeline.js
+++ react/src/Components/Main_screen_component/HourlyTimeline.js
@@ -19,16 +19,39 @@ class HourlyTimeline extends Component {
   };
 
   showHourlyComponent = () => {
-    let data = this.props.hourlydata;
-    if (this.state.pointer == 0) {
-      for (let x = 1; x < 6; x++) {
-        let each = data[x];
-        return (
-          <li className="hourly-tag">
-            <HourlyComponent time="" weather="" temp="" />
-          </li>
-        );
+    console.log(this.props.hourlydata);
+    if (this.props.hourlydata) {
+      let data = this.props.hourlydata;
+      let a,
+        limit = 0;
+      if (this.state.pointer == 0) {
+        a = 0;
+        limit = 6;
+      } else {
+        a = 6;
+        limit = 12;
+        this.state.pointer = 6;
       }
+      let Components = [];
+      for (let x = a; x < limit; x++) {
+        console.log(x + " : " + limit);
+        const each = data[x];
+        console.log(each);
+        const date = new Date(each.DateTime);
+        let time = date.getHours() + ":" + date.getMinutes();
+        let temperature = Math.round((each.Temperature.Value - 32) * (5 / 9));
+        let daylight = each.IsDayLight;
+        let obj = {
+          time: time,
+          weather: each.IconPhrase,
+          temp: temperature,
+          daylight: daylight
+        };
+        Components[x] = obj;
+      }
+      console.log("This is the object");
+      console.log(Components);
+      return Components;
     }
   };
   render() {
@@ -41,26 +64,16 @@ class HourlyTimeline extends Component {
             <li>
               <button onClick={this.previous}>{`<`}</button>
             </li>
-
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-            <li className="hourly-tag">
-              <HourlyComponent />
-            </li>
-
+            {this.showHourlyComponent().map(each => (
+              <li className="hourly-tag">
+                <HourlyComponent
+                  time={each.time}
+                  weather={each.weather}
+                  temp={each.temp}
+                  daylight={each.daylight}
+                />
+              </li>
+            ))}
             <li>
               <button onClick={this.next}>{`>`}</button>
             </li>
@@ -73,3 +86,15 @@ class HourlyTimeline extends Component {
 }
 
 export default HourlyTimeline;
+/*{this.showHourlyComponent().map(each => (
+  <li className="hourly-tag">
+    <HourlyComponent
+      time={each.time}
+      weather={each.weather}
+      temp={each.temp}
+      daylight={each.daylight}
+    />
+  </li>
+))}
+
+*/
