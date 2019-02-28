39cd61f3cbf3843ff0f6c447b406b4b628851f6f
Muhammad Sumeer <amir00505@gmail.com>
2019-02-28 23:52:21 +0000

    added bils api

--- server.js
+++ server.js
@@ -1,5 +1,76 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const readline = require("readline");
+const rstream = fs.createReadStream(
+  __dirname + "/API/1-premierleague.txt",
+  "utf8"
+);
+
+const wstream = fs.createWriteStream(__dirname + "/API/week.txt", "utf8");
+rl = readline.createInterface({
+  input: rstream,
+  terminal: false
+});
+let txt = "";
+let start = false;
+let count = 0;
+let count2 = 0;
+let i = 0;
+rl.on("line", line => {
+  if (start === true) {
+    if (!(count === 12)) {
+      if (line.includes("[")) {
+        line = line.substring(1, 11);
+        if (count2 > 0) {
+          txt += '],"' + line + '":[';
+        } else {
+          txt += '"' + line + '":[';
+        }
+        count2++;
+      } else {
+        let arr = line.split(" ");
+        arr = arr.filter(function(str) {
+          return /\S/.test(str);
+        });
+        if (txt[txt.length - 1] === "[") {
+          txt += '{ "Home": "';
+        } else {
+          txt += ',{ "Home": "';
+        }
+        for (i = 0; i < arr.length; i++) {
+          if (/\d/.test(arr[i])) {
+            break;
+          } else {
+            txt += arr[i];
+          }
+        }
+        txt += '"';
+        txt += ', "Score": ';
+        txt += '"' + arr[i] + '"';
+        i++;
+        txt += ', "Away": "';
+        for (; i < arr.length; i++) {
+          if (i == arr.length - 1) {
+            txt += arr[i];
+          } else {
+            txt += arr[i] + " ";
+          }
+        }
+        txt += '"}';
+      }
+      count++;
+    }
+  }
+  if (line === "Matchday 28") {
+    txt += '[{"' + line + '":{';
+    start = true;
+  }
+});
+rl.on("close", () => {
+  txt += "]}}]";
+  wstream.write(txt);
+});
 const app = express();
 app.use(cors());
 
