d39cee077135e700e55e294a11bf4978ab980dad
Muhammad Sumeer <amir00505@gmail.com>
2019-02-28 23:55:15 +0000

    fixed search

--- react/src/Components/Search/SearchBar.js
+++ react/src/Components/Search/SearchBar.js
@@ -5,7 +5,9 @@ class SearchBar extends Component {
   render() {
     let clubs = this.state.clubInfo.filter(club => {
       return (
-        (club.Stadium.toLowerCase().indexOf(this.state.search.toLowerCase()) &&
+        (club.Stadium.Name.toLowerCase().indexOf(
+          this.state.search.toLowerCase()
+        ) &&
           club.Club.toLowerCase().indexOf(this.state.search.toLowerCase())) !==
         -1
       );
