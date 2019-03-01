fbcaff2bba41059f7e37ab894a4fa2a1bb24d34d
Muhammad Sumeer <amir00505@gmail.com>
2019-03-01 22:43:52 +0000

    update

--- react/src/Components/Football/smresult.js
+++ react/src/Components/Football/smresult.js
@@ -5,7 +5,12 @@ class smresult extends Component {
   render() {
     return (
       <div>
-        {this.props.lol.Matchday.Tue.map((obj, i) => (
+        {this.props.lol.Matchday.a.map((obj, i) => (
+          <p key={i} className="res">
+            {obj.Home + " " + obj.Score + " " + obj.Away}
+          </p>
+        ))}
+        {this.props.lol.Matchday.b.map((obj, i) => (
           <p key={i} className="res">
             {obj.Home + " " + obj.Score + " " + obj.Away}
           </p>
