065948a2cf879dc3757b7d4efd326236f58881af
Muhammad Sumeer <amir00505@gmail.com>
2019-03-04 12:08:51 +0000

    update

--- react/src/App.js
+++ react/src/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
-import "./Styles/App.css";
-import Main from "./Components/main";
-import Search from "./Components/Search";
-import Error from "./Components/error";
-import NavBar from "./Components/navBar";
-import Football from "./Components/MainFootball";
-import Club from "./Components/mainInfo";
-import "../src/Styles/nav.css";
-import "../src/Styles/main.css";
-import "../src/Styles/InfoComponent.css";
-import "../src/Styles/StadiumInfo.css";
-import "./Styles/Search.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import './Styles/App.css';
+import Main from './Components/main';
+import Search from './Components/Search';
+import Error from './Components/error';
+import NavBar from './Components/navBar';
+import Football from './Components/MainFootball';
+import Club from './Components/mainInfo';
+import '../src/Styles/nav.css';
+import '../src/Styles/main.css';
+import '../src/Styles/InfoComponent.css';
+import '../src/Styles/StadiumInfo.css';
+import './Styles/Search.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route path="/" component={Main} exact />
-            <Route path="/search" component={Search} />
-            <Route path="/football" component={Football} />
-            <Route path="/club/:Club" component={Club} />
-            <Route component={Error} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div>
+					<NavBar />
+					<Switch>
+						<Route path='/' component={Main} exact />
+						<Route path='/search' component={Search} />
+						<Route path='/football' component={Football} />
+						<Route path='/club/:Club' component={Club} />
+						<Route component={Error} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
