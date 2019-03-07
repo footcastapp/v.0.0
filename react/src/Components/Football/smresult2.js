1189024c465fe08481c1d29603c8f30e81b7171e
Bilal <bilalimran_99@hotmail.com>
2019-03-07 18:55:47 +0000

    update

--- react/src/Components/Football/smresult2.js
+++ react/src/Components/Football/smresult2.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class smresult2 extends Component {
-	state = {};
-	render() {
-		return (
-			<div>
-				{this.props.lol.Matchday.b.map((obj, i) => (
-					<div key={i}>
-						<p className='res'>{obj.Home}</p>
-						<p className='ras'>{obj.Score}</p>
-						<p className='ros'>{obj.Away}</p>
-					</div>
-				))}
-			</div>
-		);
-	}
+  state = {};
+  render() {
+    return (
+      <div>
+        {this.props.lol.Matchday.b.map((obj, i) => (
+          <div key={i}>
+            <p className="res">{obj.Home}</p>
+            <p className="ras">{obj.Score}</p>
+            <p className="ros">{obj.Away}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default smresult2;
